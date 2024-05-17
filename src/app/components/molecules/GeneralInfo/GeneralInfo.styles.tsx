4c0aebc75842ee6f5d73152d67973a0f7f6c6674
unknown <nivekynob@gmail.com>
2024-05-17 20:19:32 +0530

    feat(tulir-site): added general info

--- src/app/components/molecules/GeneralInfo/GeneralInfo.styles.tsx
+++ src/app/components/molecules/GeneralInfo/GeneralInfo.styles.tsx
@@ -1,12 +1,48 @@
 import styled from "styled-components";
 
 export const StyledGeneralInfo = styled.div`
-    .general-info {
-        margin-top: -200px;
-        overflow-y: auto;
-        max-height: 100vh;
-        .svg-container {
-            height: 150vh;
+    .svg-container {
+        position: relative;
+        margin-top: 120px;
+        .general-info-bubble {
+            position: absolute;
+            max-width: 200px;
+            overflow: hidden;
+            .bubble-heading {
+                font-weight: 700;
+                font-size: 20px;
+                text-transform: capitalize;
+            }
+            .bubble-content {
+                text-overflow: ellipsis;
+                overflow: hidden;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 7; 
+                .bubble-list {
+                    list-style-type: disc;
+                }
+            }
+            &.famous-quote {
+                top: 12%;
+                left: 40%;
+            }
+            &.about-tulir {
+                top: 35%;
+                left: 26%;
+            }
+            &.personal-safety-rules {
+                top: 15%;
+                left: 60%;
+            }
+            &.it-takes-a-vaillage {
+                top: 37%;
+                left: 67%;
+            }
+            &.myths-facts {
+                top: 63%;
+                left: 69%;
+            }
         }
     }
 `;

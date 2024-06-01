28d2df132acbc57028549a374255859b4997ed4f
unknown <nivekynob@gmail.com>
2024-06-01 19:19:36 +0530

    added-prettier-precommit

--- src/app/mocks/AboutPage.mock.ts
+++ src/app/mocks/AboutPage.mock.ts
@@ -4,22 +4,22 @@ export const AboutPageMock = [
         tabId: 0,
         tabIdentifier: 'tulir-cphcsa',
         tabContent: [
-            "Tulir - Centre for the Prevention and Healing of Child Sexual Abuse (CPHCSA) is a registered, nongovernmental, non-profit organization committed to working against child sexual abuse in India.",
-            "Contrary to a widely held perception, a society's integrity and worth is not based on whether cases of child sexual abuse exist. They do exist, sadly they exist in silence due to a host of factors headed by the discomfort it generates if acknowledged. Instead Tulir - Centre for the Prevention and Healing of Child Sexual Abuse believes a society's integrity is based on the acceptance of the problem and proactive steps taken to respond in a timely and appropriate way ensuring that its children may benefit from its caring and foresight, and truly have the right to feel safe all the time.",
-            "While in India we are just beginning to acknowledge the existence of CSA, Tulir-CPHCSA has the audacity of hope that it will not be before long that the larger community realizes the prevalence, with the subsequent significant impact (and accompanying physical, social and health costs) for it to be considered a serious public health concern which needs to prevented and addressed.",
-            "It is a long journey, from just this acknowledgment to the actual empowering of the child and the adult stakeholders, and this website serves as just one more platform to break the silence around the issue. We hope that besides being a source of information it will link and network with individuals and organizations interested in creating a world where every child enjoys the right to feel safe all the time."
+            { hasBulletList: false, isSubHeading: false, paraText: "Tulir - Centre for the Prevention and Healing of Child Sexual Abuse (CPHCSA) is a registered, nongovernmental, non-profit organization committed to working against child sexual abuse in India." },
+            { hasBulletList: false, isSubHeading: false, paraText: "Contrary to a widely held perception, a society's integrity and worth is not based on whether cases of child sexual abuse exist. They do exist, sadly they exist in silence due to a host of factors headed by the discomfort it generates if acknowledged. Instead Tulir - Centre for the Prevention and Healing of Child Sexual Abuse believes a society's integrity is based on the acceptance of the problem and proactive steps taken to respond in a timely and appropriate way ensuring that its children may benefit from its caring and foresight, and truly have the right to feel safe all the time." },
+            { hasBulletList: false, isSubHeading: false, paraText: "While in India we are just beginning to acknowledge the existence of CSA, Tulir-CPHCSA has the audacity of hope that it will not be before long that the larger community realizes the prevalence, with the subsequent significant impact (and accompanying physical, social and health costs) for it to be considered a serious public health concern which needs to prevented and addressed." },
+            { hasBulletList: false, isSubHeading: false, paraText: "It is a long journey, from just this acknowledgment to the actual empowering of the child and the adult stakeholders, and this website serves as just one more platform to break the silence around the issue. We hope that besides being a source of information it will link and network with individuals and organizations interested in creating a world where every child enjoys the right to feel safe all the time." }
         ]
     },
     {
-        tabName: 'Mission Statement	',
+        tabName: 'Mission Statement',
         tabId: 1,
         tabIdentifier: 'mission-statement',
         tabContent: [
-            "Preventing and healing child sexual abuse require equal parts of caring, optimism and pragmatism. We must care deeply for our children, be optimistic about human being's capabilities for change and be pragmatic how to work with children and the larger community to bring about positive transformation.",
-            "Tulir-CPHCSA assumes that caring and optimism already reside within the majority of people and thus focuses on the pragmatic - how to translate the abstract notions of prevention and healing into a blueprint for action.",
-            "Prevention has increasingly gained recognition as the most prudent and cost effective strategy for addressing child sexual abuse. More importantly prevention, through the concept of Personal Safety Education, works to allow every child the right to feel safe all the time.",
-            "However, while prevention skills of children and vigilance of parents and adults do not guarantee that no child will ever be hurt, Tulir-CPHCSA believes proactivity and timeliness are the essence to an appropriate response.",
-            "Basic to Tulir-CPHCSA's philosophy of supporting abused children in their journey of healing is belief in the innate ability of the human spirit to prevail and every child's unique strengths and resilience. Furthermore Tulir-CPHCSA recognizes the significance of family and community in a child's life, and is committed to working holistically towards empowering children triumph over abuse and towards normalcy.",
+            { hasBulletList: false, isSubHeading: false, paraText: "Preventing and healing child sexual abuse require equal parts of caring, optimism and pragmatism. We must care deeply for our children, be optimistic about human being's capabilities for change and be pragmatic how to work with children and the larger community to bring about positive transformation." },
+            { hasBulletList: false, isSubHeading: false, paraText: "Tulir-CPHCSA assumes that caring and optimism already reside within the majority of people and thus focuses on the pragmatic - how to translate the abstract notions of prevention and healing into a blueprint for action." },
+            { hasBulletList: false, isSubHeading: false, paraText: "Prevention has increasingly gained recognition as the most prudent and cost effective strategy for addressing child sexual abuse. More importantly prevention, through the concept of Personal Safety Education, works to allow every child the right to feel safe all the time." },
+            { hasBulletList: false, isSubHeading: false, paraText: "However, while prevention skills of children and vigilance of parents and adults do not guarantee that no child will ever be hurt, Tulir-CPHCSA believes proactivity and timeliness are the essence to an appropriate response." },
+            { hasBulletList: false, isSubHeading: false, paraText: "Basic to Tulir-CPHCSA's philosophy of supporting abused children in their journey of healing is belief in the innate ability of the human spirit to prevail and every child's unique strengths and resilience. Furthermore Tulir-CPHCSA recognizes the significance of family and community in a child's life, and is committed to working holistically towards empowering children triumph over abuse and towards normalcy." },
         ]
     },
     {
@@ -27,11 +27,11 @@ export const AboutPageMock = [
         tabId: 2,
         tabIdentifier: 'objectives',
         tabContent: [
-            "To support and participate in local, national and international efforts to promote and protect the rights of the child.",
-            "To raise awareness on child sexual abuse.",
-            "To work towards improving policy and advancing practice to prevent and address cases of child sexual abuse with a special emphasis on the psychosocial well being of children.",
-            "To provide direct intervention services in the areas of prevention and healing of child sexual abuse.",
-            "To undertake research, documentation, and dissemination of information on child sexual abuse."
+            { hasBulletList: false, isSubHeading: false, paraText: "To support and participate in local, national and international efforts to promote and protect the rights of the child." },
+            { hasBulletList: false, isSubHeading: false, paraText: "To raise awareness on child sexual abuse." },
+            { hasBulletList: false, isSubHeading: false, paraText: "To work towards improving policy and advancing practice to prevent and address cases of child sexual abuse with a special emphasis on the psychosocial well being of children." },
+            { hasBulletList: false, isSubHeading: false, paraText: "To provide direct intervention services in the areas of prevention and healing of child sexual abuse." },
+            { hasBulletList: false, isSubHeading: false, paraText: "To undertake research, documentation, and dissemination of information on child sexual abuse." }
         ]
     }
 ]
